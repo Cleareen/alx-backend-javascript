9d9db5c7f42fb949503ff02526638e1127a98e59
EDDIE MKANSI <51905275+Cleareen@users.noreply.github.com>
2024-11-05 12:23:45 +0200

    Add files via upload

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,23 @@
+/**
+ * Prompts the user to enter their name, reads the input from STDIN,
+ * and displays a message containing the user's name.
+ *
+ * When the user ends the program, a closing message is displayed.
+ *
+ * @example
+ * // Output:
+ * // Welcome to Holberton School, what is your name?
+ * // Alice
+ * // Your name is: Alice
+ * // This important software is now closing
+ */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.setEncoding('utf8');
+
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,25 +1,36 @@
+/* eslint-disable no-param-reassign */
 const fs = require('fs');
 
-const countStudents = (path) => {
-  try {
-    let data = fs.readFileSync(path, 'utf8').toString().split('\n');
-    data = data.slice(1, data.length - 1);
-    console.log(`Number of students: ${data.length}`);
-    const fields = {};
-    for (const row of data) {
-      const student = row.split(',');
-      if (!fields[student[3]]) {
-        fields[student[3]] = [];
-      }
-      fields[student[3]].push(student[0]);
-    }
-    for (const field in fields) {
-      if (field) {
-        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-      }
-    }
-  } catch (error) {
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ * @throws {Error} If the database cannot be loaded.
+ */
+const countStudents = (dataPath) => {
+  if (!fs.existsSync(dataPath) || !fs.statSync(dataPath).isFile()) {
     throw new Error('Cannot load the database');
   }
+
+  const fileLines = fs.readFileSync(dataPath, 'utf-8').trim().split('\n');
+  const dbFieldNames = fileLines.shift().split(',');
+  const studentPropNames = dbFieldNames.slice(0, -1);
+
+  const studentGroups = fileLines.reduce((groups, line) => {
+    const studentRecord = line.split(',');
+    const studentPropValues = studentRecord.slice(0, -1);
+    const field = studentRecord[studentRecord.length - 1];
+    groups[field] = groups[field] || [];
+    // eslint-disable-next-line max-len
+    groups[field].push(Object.fromEntries(studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]])));
+    return groups;
+  }, {});
+
+  const totalStudents = Object.values(studentGroups).reduce((pre, cur) => pre + cur.length, 0);
+  console.log(`Number of students: ${totalStudents}`);
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const studentNames = group.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+  }
 };
+
 module.exports = countStudents;
